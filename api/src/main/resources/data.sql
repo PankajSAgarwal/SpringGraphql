e51973444ada1663723cbd8f55ad7ff81c57757d
Pankaj Agarwal <2005pank@gmail.com>
2021-10-23 20:57:23 +0530

    Spring Graphql sample

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into customer(name) values('Pankaj');
+insert into customer(name) values('John');
+insert into customer(name) values('Josh');
+insert into customer(name) values('Madhura');
+insert into customer(name) values('Mark');
+insert into customer(name) values('Phil');
+insert into customer(name) values('Yuxin');
